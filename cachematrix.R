dd88f9b5311b458e7dc9e05de23a6e4e2f5fcb82
krishtgkk <44180514+krishtgkk@users.noreply.github.com>
2018-11-10 06:31:53 -0500

    Completed Assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a Matrixand cacheSolve returns inverse of the matrix.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix takes a matrix as argument and returns a list of functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function(x) x
+  setInverse <- function(inversed_matrix) m <<- inversed_matrix
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the Inverse of the function if the matrix is not found in the Cache.
+## if found in cache then it returns the cached matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         m <- a$getInverse()
+  if(!is.null(m)) {
+    message("getting cached matrix data")
+    return(m)
+  }
+  data <- a$get(x)
+  m <- solve(data, ...)
+  a$setInverse(m)
+  m
 }
+# Sample matrix variable for testing
+asig_matrix <- matrix( c(1, 2, 3,4), nrow=2)
+# List of functions returned after making call to makeCacheMatrix.
+a <-makeCacheMatrix(asig_matrix)
+# calling cacheSolve function for the first time computes the inverse of already not found
+# If found data is returned from cache.
+
+cacheSolve(asig_matrix)
